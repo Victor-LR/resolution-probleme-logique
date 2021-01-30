a978d959e81456c3ff3a2eb395130a30e6c8ba3d
Izeusse <55494630+Izeusse@users.noreply.github.com>
2021-01-30 14:47:26 +0100

    gird dynamique

--- grid.py
+++ grid.py
@@ -1,23 +1,6 @@
 from tkinter import*
 
-master = Tk()
-master.geometry("500x500")
-
-text1 = Label(master, text="Premier type",wraplength=1,background="black",foreground="white")
-text1.grid(row=2, column=0, rowspan=3)
-
-text1 = Label(master, text="truc")
-text1.grid(row=2, column=1 )
-
-text1 = Label(master, text="machin")
-text1.grid(row=3, column=1 )
-
-text1 = Label(master, text="bordel")
-text1.grid(row=4, column=1 )
-
-argsSize = 3
-argsCount = 2
-
+# button custom
 class SpecialButton(Button):
     def __init__(self, master=None, **kw):
         self.state = 0
@@ -41,25 +24,51 @@ class SpecialButton(Button):
 def somethingElse():
     print("Doing something else")
 
-    
-frameSpace = Frame(master, width=20, height=300, background="Black")
-frameSpace.grid(row=0, column=2, rowspan=5)
+# size venant de minizinc apres
+arguments = 5
+variables = 4
 
-for l in range(3,11,4):
-    text2 = Label(master, text="Autre type",width=20, height=2,background="black",foreground="white")
-    text2.grid(row=0, column=l, columnspan=3)
+# grid
+master = Tk()
 
-    text1 = Label(master, text="truc",wraplength=1)
-    text1.grid(row=1, column=l )
+length = arguments * variables * 60
+height = arguments * 100
+master.geometry(str(length) + "x" + str(height))
 
-    text1 = Label(master, text="machin",wraplength=1)
-    text1.grid(row=1, column=l+1 )
 
-    text1 = Label(master, text="bordel",wraplength=1)
-    text1.grid(row=1, column=l+2 )
 
-    for i in range(2, 5):
-      for j in range(l, l+3):
+# barre horizontale gauche
+text1 = Label(master, text="Premier type",wraplength=1,height = arguments * 4,background="black",foreground="white")
+text1.grid(row=2, column=0, rowspan=arguments)
+
+# arguments de gauche
+for k in range(2,2+arguments):
+    text1 = Label(master, text="machin")
+    text1.grid(row=k, column=1 )
+
+# barre de séparation verticale de gauche
+frameSpace = Frame(master, width=20, height=arguments * 80, background="Black")
+frameSpace.grid(row=0, column=2, rowspan=2+arguments)
+
+start = 3
+step = arguments+1
+
+
+
+# parcours des différents grid
+for l in range(start,start+step*variables,step):
+    # premiere barre horizontale en haut
+    text2 = Label(master, text="Autre type",width=arguments*7, height=2,background="black",foreground="white")
+    text2.grid(row=0, column=l, columnspan=arguments)
+
+    # arguments du haut
+    for p in range(l,l+arguments):
+        text1 = Label(master, text="truc",wraplength=1)
+        text1.grid(row=1, column=p )
+
+    # grid des boutons
+    for i in range(2, 2+arguments):
+      for j in range(l, l+arguments):
             if (i % 2) == 0 and (j % 2) == 0:
                 frame1 = SpecialButton(master, width=4, height=2, background="Yellow",command = somethingElse)
                 frame1.grid(row=i, column=j)
@@ -73,8 +82,9 @@ for l in range(3,11,4):
                frame1 = SpecialButton(master, width=4, height=2, background="Yellow",command = somethingElse)
                frame1.grid(row=i, column=j)
 
-    frameSpace = Frame(master, width=20, height=300, background="Black")
-    frameSpace.grid(row=0, column=l+3, rowspan=5)
+    # séparation entre les grids
+    frameSpace = Frame(master, width=20, height=arguments * 80, background="Black")
+    frameSpace.grid(row=0, column=l+arguments, rowspan=2+arguments)
 
 
 master.mainloop()
