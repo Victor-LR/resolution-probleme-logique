1643c85d22eb446369331f878957355688ad103d
Matthias <matthias.launay@hotmail.com>
2021-02-04 20:04:03 +0100

    correction

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ inst = minizinc.Instance(gecode, model)
 
 # Solve the instance
 result = inst.solve()
+print(result)
 
-if "puzzle_logic_2.mzn" in filename:
-    arguments = {"Taille":["13'", "15'", "15'6", "21'5", "27'"],"Processeur":["2.0MHz", "2.3MHz", "2.5MHz", "2.7MHz", "3.1MHz"],"Disque":["250Gb", "320Gb", "500Gb", "750Gb", "1024Gb"],"Prix":["699$", "999$", "1149$", "1349$", "1649$"]}
-    current = {"Processeur":[0,0,0,0,0],"Disque":[0,0,0,0,0],"Prix":[0,0,0,0,0]}
+if "puzzle_logic_3.mzn" in filename:
+    arguments = {"monitor":["13'", "15'", "15'6", "21'5", "27'"],"cpu":["2.0MHz", "2.3MHz", "2.5MHz", "2.7MHz", "3.1MHz"],"hdd":["250Gb", "320Gb", "500Gb", "750Gb", "1024Gb"],"price":["699$", "999$", "1149$", "1349$", "1649$"]}
+    current = {"cpu":[0,0,0,0,0],"hdd":[0,0,0,0,0],"price":[0,0,0,0,0]}
 else:
-    arguments = {"name":["Jesssica","Laurie","Mark","Mary","Sally"],"film":["Minutes","Donnie","Scarecrow","Scarface","Recruit"],"day":["Monday","Tuesday","Wednesday","Thursday","Friday"],"time":["t735","t740","t820","t830","t845"]}
+    arguments = {"name":["Jessica","Laurie","Mark","Mary","Sally"],"film":["Minutes","Donnie","Scarecrow","Scarface","Recruit"],"day":["Monday","Tuesday","Wednesday","Thursday","Friday"],"time":["t735","t740","t820","t830","t845"]}
     current = {"film":[0,0,0,0,0],"day":[0,0,0,0,0],"time":[0,0,0,0,0]}
 
 
